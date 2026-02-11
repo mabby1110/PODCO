dc230ba08df8d40f07a625a9ca7af7e60f86ffd6
mabby1110 <rznns.mabby@gmail.com>
2026-02-11 17:12:51 -0600

    fix controls panel style

--- app/src/lib/components/Filter.svelte
+++ app/src/lib/components/Filter.svelte
@@ -6,7 +6,7 @@
 	// Variable local para el bind - debe ser string para el select
 	let keyword = $state(filterStore.keyword ?? '');
 	let profile = $state(page.data.profile);
-	let selected = $state(profile.isAdmin?'':profile.id);
+	let selected = $state(profile.isAdmin ? '' : profile.id);
 	// Sincronizar cambios de la variable local al store
 	$effect(() => {
 		// Convierte a número si no está vacío, sino ''
@@ -18,19 +18,17 @@
 	});
 </script>
 
-<div>
-	{#if profile.isAdmin}
-		<select bind:value={selected} class="butter">
-			<option value="">Todos</option>
-			{#each agentes as agente}
-				<option value={String(agente.id)}>{agente.nombre}</option>
-			{/each}
-		</select>
-	{/if}
+{#if profile.isAdmin}
+	<select bind:value={selected} class="butter">
+		<option value="">Todos</option>
+		{#each agentes as agente}
+			<option value={String(agente.id)}>{agente.nombre}</option>
+		{/each}
+	</select>
+{/if}
 
-	<!-- <input
+<!-- <input
 		type="text"
 		bind:value={keyword}
 		placeholder="Buscar oportunidades..."
 	/> -->
-</div>
