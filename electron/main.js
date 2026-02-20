569811fe1160c3554c801b1a0c461e7518acdb52
mabby1110 <rznns.mabby@gmail.com>
2026-02-19 18:13:15 -0600

    electron app builder

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const { spawn } = require('child_process');
 const path = require('path');
 
@@ -7,24 +7,32 @@ let serverProcess;
 function createWindow() {
 	const win = new BrowserWindow({
 		width: 1200,
-		height: 800
+		height: 800,
+		icon: path.join(__dirname, 'assets', 'icon.ico'),
+		autoHideMenuBar: true,
+		webPreferences: {
+			nodeIntegration: false,
+			contextIsolation: true,
+			devTools: false
+		}
 	});
 
+	Menu.setApplicationMenu(null);
 	win.loadURL('http://localhost:3000');
 }
 
 app.whenReady().then(() => {
-	serverProcess = spawn(
-		'node',
-		[path.join(__dirname, '../app/build/index.js')],
-		{
-			stdio: 'inherit',
-			env: {
-				...process.env,
-				ORIGIN: 'http://localhost:3000'
-			}
+	const serverPath = app.isPackaged
+		? path.join(process.resourcesPath, 'app/build/index.js')
+		: path.join(__dirname, '../app/build/index.js');
+
+	serverProcess = spawn('node', [serverPath], {
+		stdio: 'inherit',
+		env: {
+			...process.env,
+			ORIGIN: 'http://localhost:3000'
 		}
-	);
+	});
 
 	setTimeout(createWindow, 1500);
 });
